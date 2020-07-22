ef470d6c5e2ce5ba2873d2176fb905cba3a389cc
HongshiTan <sasuke.ths@gmail.com>
2020-07-22 23:30:34 +0800

    Update code for 3-motifs approximation

---
+++ approximation.h
@@ -0,0 +1,78 @@
+#ifndef __APPROXIMATION_H__
+#define __APPROXIMATION_H__
+
+#include "host_graph_sw.h"
+
+
+#include <boost/multiprecision/gmp.hpp>
+#include <boost/multiprecision/random.hpp>
+
+using namespace boost::multiprecision;
+using namespace boost::random;
+
+
+#define  MAX_NUM_ESTIMATOR  (1000 * 1000 * 10)
+
+
+typedef struct
+{
+    unsigned int id;
+    int node[2];
+    unsigned int p;
+    unsigned int update_counter;
+} sample_edge;
+
+
+
+typedef struct {
+    double expecation;
+    sample_edge first_edge;
+    sample_edge second_edge;
+    unsigned int neighbor_counter;
+    unsigned int neighbor_id;
+    unsigned char neighbor_flag;
+    unsigned int status;
+
+} estimator;
+
+typedef struct
+{
+    pthread_t pid;
+    estimator *est;
+    Graph* gptr;
+    CSR* csr;
+    double expecation;
+    unsigned int run;
+    unsigned int counter;
+    unsigned int est_id;
+} thread_item;
+
+
+typedef mt11213b  prng;
+
+int approximation_motifs_scheme_1(estimator *p_est, Graph* gptr, CSR* csr, int id);
+int approximation_motifs_scheme_2(estimator *p_est, Graph* gptr, CSR* csr, int id);
+
+
+int triangle_count(Graph* gptr, CSR* csr, int num);
+
+int approximation_triangle_scheme_1(estimator *p_est, Graph* gptr, CSR* csr, int id);
+int approximation_triangle_scheme_2(estimator *p_est, Graph* gptr, CSR* csr, int id);
+
+inline int reservoir_sampling(int n, prng &lmt)
+{
+    bernoulli_distribution<double> p(1.0 / (n));
+    bool res = p(lmt);
+    //DEBUG_PRINTF("n %d, res %d\n", n, res);
+    if (res)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+
+
+
+
+#endif
--- host_graph.cpp
+++ host_graph.cpp
@@ -7,56 +7,15 @@
 
 #include "host_graph_sw.h"
 
-
-#include <boost/multiprecision/gmp.hpp>
-#include <boost/multiprecision/random.hpp>
-
-using namespace boost::multiprecision;
-using namespace boost::random;
+#include "approximation.h"
 
 
-
-#define RNG_RANGE_MAX           (30)
-
-
-#define APPROXIMATE_FUNCTION         approximation_scheme_2
+#define APPROXIMATE_FUNCTION         approximation_motifs_scheme_1
 
 //
 // Generate the numbers:
 //
 
-typedef struct
-{
-    unsigned int id;
-    int node[2];
-    unsigned int p;
-    unsigned int update_counter;
-} sample_edge;
-
-
-
-typedef struct {
-    double expecation;
-    sample_edge first_edge;
-    sample_edge second_edge;
-    unsigned int neighbor_counter;
-    unsigned int neighbor_id;
-    unsigned char neighbor_flag;
-    unsigned int status;
-
-} estimator;
-
-typedef struct
-{
-    pthread_t pid;
-    estimator *est;
-    Graph* gptr;
-    CSR* csr;
-    double expecation;
-    unsigned int run;
-    unsigned int counter;
-    unsigned int est_id;
-} thread_item;
 
 #define MAX_NUN_THREADS 16
 thread_item   threads[MAX_NUN_THREADS];
@@ -69,175 +28,8 @@ thread_item   threads[MAX_NUN_THREADS];
 #define STATUS_END              (3)
 
 
-estimator local_estimator[MAX_NUM_ESTIMATOR];
+estimator local_estimator[MAX_NUN_THREADS];
 
-uniform_int_distribution<mpz_int> ui(0, mpz_int(1) << RNG_RANGE_MAX);
-
-const double ratio = 1.0 / (double(1 << RNG_RANGE_MAX));
-
-typedef mt11213b  prng;
-
-inline int reservoir_sampling(int n, prng &lmt)
-{
-    bernoulli_distribution<double> p(1.0 / (n));
-    bool res = p(lmt);
-    //DEBUG_PRINTF("n %d, res %d\n", n, res);
-    if (res)
-    {
-        return 1;
-    }
-    return 0;
-}
-int approximation_scheme_2(estimator *p_est, Graph* gptr, CSR* csr, int id)
-{
-    prng mt;
-    static int counter = 0;
-    mt.seed(static_cast<unsigned int>(std::time(0)) + (unsigned long)(p_est) + counter);
-
-    counter ++;
-    memset(p_est, 0, sizeof(estimator));
-    int edgeNum   = csr ->edgeNum;
-    uniform_int_distribution<mpz_int> ui(0, edgeNum - 1);
-
-    sample_edge * p_first    = &p_est->first_edge;
-    sample_edge * p_second    = &p_est->second_edge;
-
-
-    int start_edge = static_cast<int>(ui(mt));
-    p_est->first_edge.node[0] = gptr->data[start_edge][0];
-    p_est->first_edge.node[1] = gptr->data[start_edge][1];
-    p_est->first_edge.id = start_edge;
-    p_first->update_counter ++;
-    p_est->neighbor_counter = 0;
-    p_est->status = 1;
-    int temp_neighbor_counter = 0;
-
-
-
-    for (int i = start_edge + 1; i < edgeNum; i++)
-    {
-        int adjacent_flag = 0;
-        int ln = gptr->data[i][0];
-        int rn = gptr->data[i][1];
-
-#if 1
-        if ((ln == p_first->node[0]) || (rn == p_first->node[0]))
-        {
-            p_est->neighbor_flag = 1;
-            adjacent_flag = 1;
-        }
-#endif
-#if 1
-        if ((ln == p_first->node[1]) || (rn == p_first->node[1]))
-        {
-            p_est->neighbor_flag = 0;
-            adjacent_flag = 1;
-        }
-#endif
-        if (adjacent_flag == 1)
-        {
-            temp_neighbor_counter ++;
-        }
-    }
-    p_est->status = 3;
-    if (p_est->status == 3)
-    {
-        p_est->expecation = temp_neighbor_counter  * edgeNum;
-    }
-    else
-    {
-        p_est->expecation = 0;
-    }
-    return  0;
-}
-
-
-int approximation_scheme_1(estimator *p_est, Graph* gptr, CSR* csr, int id)
-{
-    prng mt;
-    static int counter = 0;
-    mt.seed(static_cast<unsigned int>(std::time(0)) + (unsigned long)(p_est) + counter);
-    counter ++;
-    memset(p_est, 0, sizeof(estimator));
-    int edgeNum   = csr ->edgeNum;
-
-    sample_edge * p_first    = &p_est->first_edge;
-    sample_edge * p_second    = &p_est->second_edge;
-
-    p_est->status = 0;
-    p_est->expecation = 0;
-
-    int start_index = 0;//edgeNum / 200 * (id % 100) + id ;
-    int temp_neighbor_counter  = 0;
-    for (int i = start_index; i < edgeNum; i++)
-    {
-        if (reservoir_sampling(i + 1, mt) )
-        {
-            p_est->first_edge.node[0] = gptr->data[i][0];
-            p_est->first_edge.node[1] = gptr->data[i][1];
-            p_first->update_counter ++;
-            temp_neighbor_counter = 0;
-            p_est->neighbor_counter = 0;
-            p_est->status = 1;
-            p_est->first_edge.id = i;
-        }
-        else
-        {
-            int ln = gptr->data[i][0];
-            int rn = gptr->data[i][1];
-
-
-            int adjacent_flag = 0;
-#if 0
-            if ((ln == p_first->node[0]))
-            {
-                p_est->neighbor_flag = 1;
-                adjacent_flag = 1;
-            }
-#endif
-#if 1
-            if ((ln == p_first->node[0]) || (rn == p_first->node[0]))
-            {
-                p_est->neighbor_flag = 1;
-                adjacent_flag = 1;
-            }
-#endif
-#if 1
-            if ((ln == p_first->node[1]) || (rn == p_first->node[1]))
-            {
-                p_est->neighbor_flag = 0;
-                adjacent_flag = 1;
-            }
-#endif
-            if (adjacent_flag == 1)
-            {
-                temp_neighbor_counter ++;
-#if 0
-                if (reservoir_sampling(temp_neighbor_counter, mt))
-                {
-                    p_est->second_edge.id = i;
-                    p_est->neighbor_counter = temp_neighbor_counter;
-                    p_est->status = 3;
-                }
-                else
-                {
-                    p_est->neighbor_counter ++;
-                }
-#endif
-            }
-        }
-    }
-    p_est->status = 3;
-    if (p_est->status == 3)
-    {
-        p_est->expecation = temp_neighbor_counter  * edgeNum;
-    }
-    else
-    {
-        p_est->expecation = 0;
-    }
-    return  0;
-}
 
 void *approximation_thread(void *argv)
 {
@@ -245,6 +37,49 @@ void *approximation_thread(void *argv)
     //DEBUG_PRINTF("here\n");
     thread_item *p_data = (thread_item *)argv;
     APPROXIMATE_FUNCTION(p_data->est, p_data->gptr, p_data->csr, p_data->est_id);
+    return 0;
+}
+
+
+int rng_test(CSR* csr, int num)
+{
+    prng mt;
+    static int counter = 0;
+    mt.seed(static_cast<unsigned int>(std::time(0))  + counter);
+
+    counter ++;
+    int edgeNum   = csr ->edgeNum;
+    uniform_int_distribution<mpz_int> ui(0, edgeNum - 1);
+    for (int i = 0; i < num; i ++)
+    {
+        int loc = static_cast<int>(ui(mt));
+        DEBUG_PRINTF("rng %d %d \n", loc , edgeNum);
+    }
+    return 0;
+}
+
+int rng_res_test(CSR* csr, int num)
+{
+    prng mt;
+    static int counter = 0;
+    mt.seed(static_cast<unsigned int>(std::time(0))  + counter);
+
+    counter ++;
+    int edgeNum   = csr ->edgeNum;
+    uniform_int_distribution<mpz_int> ui(0, edgeNum - 1);
+    for (int i = 0; i < num; i ++)
+    {
+        int loc = 0;
+        for (int k = 0; k < edgeNum; k ++)
+        {
+            if (reservoir_sampling(k + 1, mt) )
+            {
+                loc = k;
+            }
+        }
+
+        DEBUG_PRINTF("rng %d %d \n", loc , edgeNum);
+    }
     return 0;
 }
 
@@ -269,43 +104,39 @@ int main(int argc, char **argv) {
     int est_num = (argc < 4) ? EST_NUM : std::stoi(argv[3]);
     int printf_flag = std::stoi(argv[1]);
     std::string mode = "normal";
-#if 0
-    Graph* gptr = createGraph(gName, mode);
-    CSR* csr    = new CSR(*gptr);
-    csr->save2File(gName);
-    free(gptr);
-    return 0;
-#endif
 
     DEBUG_PRINTF("start main\n");
 
     std::cout << std::hex << std::showbase;
-    std::cout << (mpz_int(1) << RNG_RANGE_MAX) << std::endl;
 
     Graph* gptr = createGraph(gName, mode);
     CSR* csr    = new CSR(*gptr);
 
     int edgeNum   = csr ->edgeNum;
-    /*
-        prng mt;
-        mt.seed(static_cast<unsigned int>(std::time(0)));
-        int id = 0;
-        for (int i = 0 ; i < edgeNum ; i++)
-        {
-            if (reservoir_sampling(i + 1,mt))
-            {
-                id = i;
-            }
-            //DEBUG_PRINTF("id %d \n", id);
-        }
-        return 0;
-    */
-//ARRAY_SIZE(local_estimator)
+
+    //rng_res_test(csr, 100000);
+    //return 0;
+
+   //1465313402
+
+
     int est_num_map [] = { 16, 100, 1000, 2000,
                            5000, 10000, 20000, 50000,
                            100000, 200000, 500000, 1000 * 1000,
                            1000 * 1000 * 2, 1000 * 1000 * 5
                          };
+#if 0
+    const int total_k = ARRAY_SIZE(est_num_map) - 3;
+    for (int k = 0; k < total_k ; k ++)
+    {
+        est_num = est_num_map[k];
+        triangle_count(gptr, csr, est_num);
+    }
+
+    free(gptr);
+    free(csr);
+    return 0;
+#else
     //ARRAY_SIZE(est_num_map)
     int est_id = 0;
     const int total_k = ARRAY_SIZE(est_num_map);
@@ -349,7 +180,7 @@ int main(int argc, char **argv) {
                     sample_edge * p_second = &p_est->second_edge;
                     if  (printf_flag == 1)
                     {
-                        DEBUG_PRINTF("%d nc %d (%d %d) %d-[%d %d] %d-[%d %d]\n",
+                        DEBUG_PRINTF("%d nc %d (%d %d) %d-[%d %d] %d-[%d %d] --> %f\n",
                                      p_est->status ,
                                      p_est->neighbor_counter,
                                      p_first->update_counter,
@@ -359,7 +190,8 @@ int main(int argc, char **argv) {
                                      p_first->node[1],
                                      p_second->id,
                                      p_second->node[0],
-                                     p_second->node[1]);
+                                     p_second->node[1],
+                                     p_est->expecation);
                     }
                 }
 
@@ -395,5 +227,6 @@ int main(int argc, char **argv) {
     free(csr);
 
     return 0;
+#endif
 }
 
