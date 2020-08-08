98297bcfcb13e47ae7ea27e9e96598d6c00ae0e3
HongshiTan <sasuke.ths@gmail.com>
2020-08-09 00:22:59 +0800

    Update

--- host_graph.cpp
+++ host_graph.cpp
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
         printf("\n mutex init failed\n");
         return 1;
     }
-
+    /* TODO: replace by auto-generation */
     int est_num_map [] = {  1,        2,        5,
                             10,       20,       50, 
                             100,      200,      500, 
